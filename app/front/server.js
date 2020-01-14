d9b32ca3026169a723c2dbc10edcf7e6c1c60094
samy <samy_leni@hotmail.fr>
2020-01-14 08:57:42 +0100

    feat(global): Add gitignore and app structure

---
+++ app/front/server.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express();
+
+app.set('view engine', 'pug');
+
+app.get('/', (req, res) => {
+    res.render('index', {
+        title: 'Hey',
+        message: 'Hello there!'
+    })
+});
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'));
